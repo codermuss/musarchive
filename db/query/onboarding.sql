6d6f561ccf5dc1234529a819e4fbe9df0b4305d9
codermuss <codermuss@gmail.com>
2024-07-20 09:22:37 +0300

    ✨ edit onboarding sql

--- db/query/onboarding.sql
+++ db/query/onboarding.sql
@@ -1,19 +1,19 @@
 -- name: InsertOnboarding :one
-INSERT INTO "onboarding" ("image", "title", "description") 
+INSERT INTO onboarding (image, title, description) 
 VALUES ($1, $2, $3) 
 RETURNING *;
 
 -- name: GetOnboarding :one
-SELECT "id", "image", "title", "description" 
-FROM "onboarding" 
-WHERE "id" = $1;
+SELECT id, image, title, description 
+FROM onboarding
+WHERE id = $1;
 
 -- name: UpdateOnboarding :one
-UPDATE "onboarding" 
-SET "image" = $1, "title" = $2, "description" = $3
-WHERE "id" = $4
+UPDATE onboarding
+SET image = $1, title = $2, description = $3
+WHERE id = $4
 RETURNING *;
 
 -- name: DeleteOnboarding :exec
-DELETE FROM "onboarding" 
-WHERE "id" = $1;
+DELETE FROM onboarding 
+WHERE id = $1;
