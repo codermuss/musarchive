94f6e7fb1a578318e286b3bc20ac2553d0459057
codermuss <codermuss@gmail.com>
2024-07-28 20:21:19 +0300

    ✨ add get posts test

--- api/post_test.go
+++ api/post_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -134,6 +135,182 @@ func TestCreatePostAPI(t *testing.T) {
 			tc.checkResponse(recorder)
 		})
 	}
+}
+
+func TestGetPostsAPI(t *testing.T) {
+	user, _ := RandomUser(t)
+	posts := []db.Post{
+		randomPost(user.ID),
+		randomPost(user.ID),
+		randomPost(user.ID),
+	}
+
+	testCases := []struct {
+		name          string
+		queryParams   map[string]string
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			queryParams: map[string]string{
+				"page":   "1",
+				"size":   "5",
+				"locale": "en",
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, int(user.ID), user.Role, time.Minute)
+			},
+
+			body: gin.H{
+				"categories": []int32{1, 2},
+				"tags":       []int32{3, 4},
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.GetPostsWithFilterParams{
+					Column3: []int32{1, 2},
+					Column4: []int32{3, 4},
+					Limit:   5,
+					Offset:  0,
+				}
+
+				store.EXPECT().GetPostsWithFilter(gomock.Any(), gomock.Eq(arg)).Times(1).Return(posts, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchPosts(t, recorder.Body, posts)
+			},
+		},
+		{
+			name: "InternalError",
+			queryParams: map[string]string{
+				"page":   "1",
+				"size":   "5",
+				"locale": "en",
+			},
+
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, int(user.ID), user.Role, time.Minute)
+			},
+			body: gin.H{
+				"categories": []int32{1, 2},
+				"tags":       []int32{3, 4},
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetPostsWithFilter(gomock.Any(), gomock.Any()).Times(1).Return(nil, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidPage",
+			queryParams: map[string]string{
+				"page":   "invalid_page",
+				"size":   "5",
+				"locale": "en",
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, int(user.ID), user.Role, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetPostsWithFilter(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidSize",
+			queryParams: map[string]string{
+				"page":   "1",
+				"size":   "invalid_size",
+				"locale": "en",
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, int(user.ID), user.Role, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetPostsWithFilter(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidBody",
+			queryParams: map[string]string{
+				"page":   "1",
+				"size":   "5",
+				"locale": "en",
+			},
+
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, int(user.ID), user.Role, time.Minute)
+			},
+			body: gin.H{
+				"categories": "invalid_categories",
+				"tags":       "invalid_tags",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetPostsWithFilter(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/v1/posts"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+			server.Router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+
+			// Add query parameters
+			q := request.URL.Query()
+			for k, v := range tc.queryParams {
+				q.Add(k, v)
+			}
+			request.URL.RawQuery = q.Encode()
+
+			request.Body = io.NopCloser(bytes.NewReader(data))
+
+			server.Router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+func requireBodyMatchPosts(t *testing.T, body *bytes.Buffer, posts []db.Post) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotPosts []db.Post
+	err = json.Unmarshal(data, &gotPosts)
+	require.NoError(t, err)
+	require.Equal(t, posts, gotPosts)
 }
 
 func randomPost(id int32) db.Post {
